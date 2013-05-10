9e0ca79d95209d6ae0e8d51b7fec6079f1d029bb
furiousanger <dani.yudy@gmail.com>
2013-05-10 19:08:01 +0200

    add script to zip

---
+++ zip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+	
+cp arch/arm/boot/zImage mkboot/
+
+cd mkboot
+chmod 744 boot.img-ramdisk/sbin/lkflash
+chmod 744 boot.img-ramdisk/sbin/checkv
+chmod 744 boot.img-ramdisk/sbin/checkt
+echo "making boot image"
+./img.sh
+
+zipfile="furiousKernel-maguro-4.2_v1.0.zip"
+if [ ! $4 ]; then
+	rm -f /tmp/*.img
+	echo "making zip file"
+	cp boot.img ../zip
+	cp boot.img /tmp
+	cd ../zip
+	rm -f *.zip
+	zip -r $zipfile *
+	rm -f /tmp/*.zip
+	cp *.zip /tmp
+fi
+
